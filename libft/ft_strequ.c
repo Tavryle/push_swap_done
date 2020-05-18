fdb95d38ee9d9b7ab115b627920e8eb74f36d06b
Tavryle <tmansing@student.wethinkcode.co.za>
2020-05-18 08:02:11 -0400

    ft_strequ fixed

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -12,14 +12,9 @@
 
 #include "libft.h"
 
-int		ft_strequ(char const *s1, char const *s2)
+int	ft_strequ(char const *s1, char const *s2)
 {
-	if (s1 && s2)
-	{
-		if (s1 == s2)
-			return (1);
-		else
-			return (0);
-	}
-	return (0);
+	if (!s1 || !s2)
+		return (0);
+	return (ft_strcmp(s1, s2) == 0);
 }
